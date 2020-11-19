7c5c6216625d555197540dd2025e688d60e5e015
Jubin Soni <jubin@chartmetric.com>
2020-11-19 01:30:12 -0800

    Updated the test cases
    Updated the test cases

--- fruitpal/tests.py
+++ fruitpal/tests.py
@@ -1,16 +1,35 @@
 from requests import request
+import unittest
 
 BASE = "http://localhost:5001/"
 
 
-response = request(method='GET', url=BASE + "api/fruitpal?commodity=mango&price=53&volume=405")
-print(response.json())
+class TestFruitpal(unittest.TestCase):
 
-response = request(method='GET', url=BASE + "api/fruitpal?commodity=orange&price=35&volume=500")
-print(response.json())
-#
-response = request(method='GET', url=BASE + "api/fruitpal?commodity=apple&price=45&volume=100")
-print(response.json())
+    def test_given_example(self):
+        response = request(method='GET', url=BASE + "api/fruitpal?commodity=mango&price=53&volume=405")
+        self.assertEqual(response.status_code, 200)
+        expected = {'obj': {'data': [{'country': 'BR', 'total_price': 22040.1}, {'country': 'MX', 'total_price': 21967.2}], 'length': 2}}
+        self.assertEqual(response.json(), expected)
 
-response = request(method='GET', url=BASE + "api/fruitpal?commodity=grape&price=4r5&volume=100")
-print(response.json())
+    def test_big_response(self):
+        response = request(method='GET', url=BASE + "api/fruitpal?commodity=orange&price=35&volume=500")
+        self.assertEqual(response.status_code, 200)
+        expected = {'obj': {'data': [{'country': 'GB', 'total_price': 18525.0}, {'country': 'US', 'total_price': 18475.0}, {'country': 'BR', 'total_price': 18090.0}, {'country': 'RU', 'total_price': 18060.0}, {'country': 'MX', 'total_price': 18025.0}, {'country': 'CN', 'total_price': 17725.0}], 'length': 6}}
+        self.assertEqual(response.json(), expected)
+
+    def test_small_response(self):
+        response = request(method='GET', url=BASE + "api/fruitpal?commodity=apple&price=45&volume=100")
+        self.assertEqual(response.status_code, 200)
+        expected = {'obj': {'data': [{'country': 'GB', 'total_price': 4598.0}], 'length': 1}}
+        self.assertEqual(response.json(), expected)
+
+    def test_invalid_params(self):
+        response = request(method='GET', url=BASE + "api/fruitpal?commodity=grape&price=4r5&volume=100")
+        self.assertEqual(response.status_code, 400)
+        expected = {'message': {'price': 'Price of the commodity is required and must be an integer.'}}
+        self.assertEqual(response.json(), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
